b1b5363ae740f8b09e1adc0bf6bba8b1076916b3
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-14 23:47:08 +0100

    added reset link and bootstrap / custom css style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Defined the application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defined the root path route ("/") - TODO
-  # root "articles#index" - TODO
   get '/ask', to: 'questions#ask'
   get '/answer', to: 'questions#answer'
+  # TODO, Defines the root path route ("/")
+  # TODO,  root "articles#index"
 end
