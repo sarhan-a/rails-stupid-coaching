0fce6c90611dc54cb0432d5c0a4ca92eb361b19d
Sarhan Alim <sarhan.alim@gmail.com>
2022-07-14 19:10:26 +0100

    defined the application routes to questions_controller and added 2 views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defined the application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defined the root path route ("/") - TODO
+  # root "articles#index" - TODO
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
